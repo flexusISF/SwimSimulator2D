711f95037145b2e68db474285a82c663689e8f4b
flexusISF <146860748+flexusISF@users.noreply.github.com>
2024-05-21 10:25:35 -0700

    Day 5
    Itch build is up with Devlog and player death almost works

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -15,7 +15,7 @@ public class CarSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     void Cars()
     {
---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = GameObject.Find("RespawnPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
