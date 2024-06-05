63f88499b425b4a698abc98b69b2126f8302c1df
flexusISF <146860748+flexusISF@users.noreply.github.com>
2024-06-05 10:15:34 -0700

    Drift controller in

---
+++ Assets/Scripts/DriftController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class DriftController : MonoBehaviour
+{
+    [Header("Car settings")]
+    public float driftFactor = 0.95f;
+    public float accelerationFactor = 30.0f;
+    public float turnfactor = 3.5f;
+
+    //Local variables
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+    //Components
+    Rigidbody2D carRigidbody2D;
+
+    //Awake is called when the script instace is being loaded.
+    void Awake()
+    {
+        carRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Frame - rate independent for physics calculations 
+    void FixedUpdate()
+    {
+        ApplyEngineForce();
+
+        killOrthogonalVelocity();
+
+        ApplySteering();
+    }
+    void ApplyEngineForce()
+    {
+        //Create a force for the engine
+        Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
+
+        //Apply force and pushes the car forward
+        carRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    void ApplySteering()
+    {
+        //Limit the cars ability to turn when moving slowly
+        float minSpeedBeforeAllowTurningFactor = (carRigidbody2D.velocity.magnitude / 8);
+        minSpeedBeforeAllowTurningFactor = Mathf.Clamp01(minSpeedBeforeAllowTurningFactor);
+        //Update the rotation angle based on input
+        rotationAngle -= steeringInput * turnfactor * minSpeedBeforeAllowTurningFactor;
+
+        //Apply steering by rotating the car object
+        carRigidbody2D.MoveRotation(rotationAngle);
+    }
+
+    void killOrthogonalVelocity()
+    {
+        Vector2 forwardVeloctiy = transform.up * Vector2.Dot(carRigidbody2D.velocity, transform.up);
+        Vector2 rightVeloctiy = transform.right * Vector2.Dot(carRigidbody2D.velocity, transform.right);
+
+        carRigidbody2D.velocity = forwardVeloctiy + rightVeloctiy * driftFactor;
+    }
+    public void setInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+}
